a213e134d3bf4e8c058b8bed71bd4c006161f591
hudsonlenz <hudson.lenz123@gmail.com>
2026-04-19 07:38:49 +0000

    fix: corrigir base path para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/AgroAluga/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
